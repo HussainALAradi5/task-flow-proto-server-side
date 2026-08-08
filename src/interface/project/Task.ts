e9a56287c38eacef280bc1f80a960b6cc59a3f4c
HussainALAradi5 <hussainaradi.ha@gmail.com>
2026-08-08 15:11:19 +0300

    add the npm run build thing

--- src/interface/project/Task.ts
+++ src/interface/project/Task.ts
@@ -1,12 +1,12 @@
 import { Schema } from "mongoose";
 import { IBaseEntity } from "../BaseModel";
-import { GenericStatus } from "../../enums/project/GenericStatus";
+import { GenericStatus } from "../../enums/GenericStatus";
 import { TaskPriority } from "../../enums/project/TaskEnum";
 
 export interface ITask extends IBaseEntity {
   title: string;
   description?: string;
-  status: GenericStatus
+  status: GenericStatus;
   priority: TaskPriority;
   projectId: Schema.Types.ObjectId;
   milestoneId?: Schema.Types.ObjectId;
--- src/models/project/Task.ts
+++ src/models/project/Task.ts
@@ -1,7 +1,7 @@
 import { model, Schema } from "mongoose";
 import { ITask } from "../../interface/project/Task";
 import { baseSchemaFields } from "../BaseModel";
-import { GenericStatus } from "../../enums/project/GenericStatus";
+import { GenericStatus } from "../../enums/GenericStatus";
 import { TaskPriority } from "../../enums/project/TaskEnum";
 
 const taskSchema = new Schema<ITask>(
