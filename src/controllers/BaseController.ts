7df6cf2b0afa5ad28cde15a79139e8cc2a8d0331
HussainALAradi5 <hussainaradi.ha@gmail.com>
2026-08-08 15:34:19 +0300

    implement the basecontroller

---
+++ src/controllers/BaseController.ts
@@ -0,0 +1,34 @@
+import { Model, QueryFilter, UpdateQuery } from "mongoose";
+import * as mongoose from "mongoose";
+
+export class BaseService<T extends mongoose.Document> {
+  protected model: Model<T>;
+
+  constructor(model: Model<T>) {
+    this.model = model;
+  }
+
+  async create(data: Partial<T>): Promise<T> {
+    const record = new this.model(data);
+    return await record.save();
+  }
+
+  async getById(id: string): Promise<T | null> {
+    return await this.model.findById(id).exec();
+  }
+
+  async getAll(filter: QueryFilter<T> = {}): Promise<T[]> {
+    return await this.model.find(filter).exec();
+  }
+
+  async update(id: string, data: UpdateQuery<T>): Promise<T | null> {
+    return await this.model.findByIdAndUpdate(id, data, { 
+      new: true, 
+      runValidators: true 
+    }).exec();
+  }
+
+  async delete(id: string): Promise<T | null> {
+    return await this.model.findByIdAndDelete(id).exec();
+  }
+}
