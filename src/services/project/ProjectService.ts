aaab8295d49d01765993743cead783db8cb8155d
HussainALAradi5 <hussainaradi.ha@gmail.com>
2026-08-09 21:51:52 +0300

    add fucntrions in order to have clean efficent code

--- src/services/project/ProjectService.ts
+++ src/services/project/ProjectService.ts
@@ -1,10 +1,24 @@
 import { IProject } from '../../interface/project/Project';
 import { Project } from '../../models/project/Project';
 import { BaseService } from '../BaseService';
+import { UserService } from '../user/UserService';
 
 class ProjectServiceClass extends BaseService<IProject> {
   constructor() {
     super(Project);
   }
+
+  // Overriding create to handle project creation and ensure creator becomes a Leader
+  async createProject(data: Partial<IProject>): Promise<IProject> {
+    const project = await this.create(data);
+
+    // If a creator is attached, elevate their role if they are currently just a Member
+    if (project.createdBy) {
+      await UserService.promoteToLeaderForNewProject(project.createdBy.toString());
+    }
+
+    return project;
+  }
 }
+
 export const ProjectService = new ProjectServiceClass();
--- src/services/user/UserService.ts
+++ src/services/user/UserService.ts
@@ -1,10 +1,33 @@
 import { IUser } from '../../interface/user/User';
 import { User } from '../../models/user/User';
 import { BaseService } from '../BaseService';
+import { UserRole } from '../../enums/user/UserRoleEnum';
 
 class UserServiceClass extends BaseService<IUser> {
   constructor() {
     super(User);
   }
+
+  async create(data: Partial<IUser>): Promise<IUser> {
+    const userData: Partial<IUser> = {
+      ...data,
+      role: data.role || UserRole.MEMBER,
+    };
+    return await super.create(userData);
+  }
+
+  async assignRole(userId: string, role: UserRole): Promise<IUser | null> {
+    return await this.update(userId, { role } as any);
+  }
+
+  // Automatically elevate user to LEADER when they trigger/create a project context
+  async promoteToLeaderForNewProject(userId: string): Promise<IUser | null> {
+    const user = await this.getById(userId);
+    if (user && user.role === UserRole.MEMBER) {
+      return await this.assignRole(userId, UserRole.LEADER);
+    }
+    return user;
+  }
 }
+
 export const UserService = new UserServiceClass();
